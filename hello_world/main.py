ad0d8589bbd92b141526f4c3e2689f0914b07b2d
Robin Graf <robingraf2510@gmail.com>
2023-01-19 01:50:22 +0100

    progress

--- hello_world/main.py
+++ hello_world/main.py
@@ -30,33 +30,14 @@ def start():
     return render_template("bodyvalues_list.html", liste=neue_liste)
 
 
-@app.route("/training", methods=["GET", "POST"])
-def training():
+@app.route("/jogging", methods=["GET", "POST"])
+def jogging():
     if request.method == "GET":
         auswahl = ["Robin"]
         auswahl_username = ["grafrob"]
         ausgewaehlter_username = random.choice(auswahl_username)
         ausgewaehlter_name = random.choice(auswahl)
-        return render_template('training.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Training")
-
-@app.route("/statistik_kategorie", methods=["GET", "POST"])
-def statistik_kategorie():
-    if request.method == "GET":
-        auswahl = ["Robin"]
-        auswahl_username = ["grafrob"]
-        ausgewaehlter_username = random.choice(auswahl_username)
-        ausgewaehlter_name = random.choice(auswahl)
-        return render_template('statistik_kategorie.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Statistik Kategorie")
-
-
-@app.route("/listen_kategorie", methods=["GET", "POST"])
-def listen_kategorie():
-    if request.method == "GET":
-        auswahl = ["Robin"]
-        auswahl_username = ["grafrob"]
-        ausgewaehlter_username = random.choice(auswahl_username)
-        ausgewaehlter_name = random.choice(auswahl)
-        return render_template('listen_kategorie.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Listen Kategorie")
+        return render_template('jogging.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Jogging")
 
 
 @app.route("/statistik", methods=["GET", "POST"])
@@ -193,7 +174,6 @@ def homescreen2():
             if 'löschen' in request.form and "nummer_del" in request.form:
                 nummer_del = request.form['nummer_del']
                 loeschen(nummer_del)
-                print("haher")
 
                 inhalt_string = auslesen()
                 inhalt = ast.literal_eval(str(inhalt_string))
@@ -254,6 +234,94 @@ def homescreen2():
         ausgewaehlter_name = random.choice(auswahl)
         return render_template('bodyvalues_list.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Liste Bodyvalues", liste=neue_liste)
 
+
+
+@app.route("/liste_jogging", methods=["GET", "POST"])
+def liste_jogging():
+    if request.method == "GET":
+        inhalt_string = auslesen()
+        inhalt = ast.literal_eval(str(inhalt_string))
+        neue_liste = []
+        for eintrag in inhalt.values():
+            test = {}
+            test.update(eintrag)
+            neue_liste2 = []
+            for bezeichnung, wert in test.items():
+                neue_liste2.append([bezeichnung, wert])
+            neue_liste.append(neue_liste2)
+
+        auswahl = ["Robin"]
+        auswahl_username = ["grafrob"]
+        ausgewaehlter_username = random.choice(auswahl_username)
+        ausgewaehlter_name = random.choice(auswahl)
+        return render_template('liste_Jogging.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Liste Jogging", liste=neue_liste)
+
+    if request.method == "POST":
+        inhalt_string_len = auslesen()
+        inhalt_len = ast.literal_eval(str(inhalt_string_len))
+        if len(inhalt_len) >= 2:
+            if 'löschen' in request.form and "nummer_del" in request.form:
+                nummer_del = request.form['nummer_del']
+                loeschen(nummer_del)
+
+                inhalt_string = auslesen()
+                inhalt = ast.literal_eval(str(inhalt_string))
+                neue_liste = []
+                for eintrag in inhalt.values():
+                    test = {}
+                    test.update(eintrag)
+                    neue_liste2 = []
+                    for bezeichnung, wert in test.items():
+                        neue_liste2.append([bezeichnung, wert])
+                    neue_liste.append(neue_liste2)
+
+            elif 'bearbeiten' in request.form and "nummer_del" in request.form:
+                nummer_del = request.form['nummer_del']
+                loeschen_edit(nummer_del)
+
+                dict_del_string = auslesen_del()
+                dict_del = ast.literal_eval(str(dict_del_string))
+                neue_liste = []
+                for eintrag in dict_del.values():
+                    test = {}
+                    test.update(eintrag)
+                    neue_liste2 = []
+                    for bezeichnung, wert in test.items():
+                        neue_liste2.append([bezeichnung, wert])
+                    neue_liste.append(neue_liste2)
+
+                return redirect("http://127.0.0.1:5000/bodyvalues_edit", code=302)
+
+
+            else:
+                    inhalt_string = auslesen()
+                    inhalt = ast.literal_eval(str(inhalt_string))
+                    neue_liste = []
+                    for eintrag in inhalt.values():
+                        test = {}
+                        test.update(eintrag)
+                        neue_liste2 = []
+                        for bezeichnung, wert in test.items():
+                            neue_liste2.append([bezeichnung, wert])
+                        neue_liste.append(neue_liste2)
+
+        else:
+            inhalt_string = auslesen()
+            inhalt = ast.literal_eval(str(inhalt_string))
+            neue_liste = []
+            for eintrag in inhalt.values():
+                test = {}
+                test.update(eintrag)
+                neue_liste2 = []
+                for bezeichnung, wert in test.items():
+                    neue_liste2.append([bezeichnung, wert])
+                neue_liste.append(neue_liste2)
+
+        auswahl = ["Robin"]
+        auswahl_username = ["grafrob"]
+        ausgewaehlter_username = random.choice(auswahl_username)
+        ausgewaehlter_name = random.choice(auswahl)
+        return render_template('liste_jogging.html', name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Liste Jogging", liste=neue_liste)
 
 
 
@@ -426,6 +494,177 @@ def grafik():
 
 
     return render_template("viz.html", barchart_gewicht=div1, barchart_bodyfat=div2, barchart_tbw=div3, barchart_muskeln=div4, barchart_bmi=div5, barchart_fettgewicht=div6, barchart_muskelgewicht=div7, name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Statistik Bodyvalues")
+
+
+
+@app.route("/statistik_jogging")
+def statistik_jogging():
+    if request.method == "GET":
+        inhalt_string = auslesen()
+        inhalt = ast.literal_eval(str(inhalt_string))
+        neue_liste = []
+        for eintrag in inhalt.values():
+            test = {}
+            test.update(eintrag)
+            neue_liste2 = []
+            for bezeichnung, wert in test.items():
+                neue_liste2.append([bezeichnung, wert])
+            neue_liste.append(neue_liste2)
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[1][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        Gewicht=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="Gewicht", title="Gewicht in kg")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div1 = plot(fig, output_type="div")
+
+    ######################################################################################
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[2][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        Bodyfat=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="Bodyfat", title="Bodyfat in %")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div2 = plot(fig, output_type="div")
+
+    ######################################################################################
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[3][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        Körperwasser=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="Körperwasser", title="Körperwasser in %")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div3 = plot(fig, output_type="div")
+
+    ######################################################################################
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[4][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        Muskeln=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="Muskeln", title="Muskeln in %")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div4 = plot(fig, output_type="div")
+
+    ######################################################################################
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[5][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        BMI=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="BMI", title="BMI")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div5 = plot(fig, output_type="div")
+
+    ######################################################################################
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[7][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        Fettgewicht=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="Fettgewicht", title="Fettgewicht in kg")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div6 = plot(fig, output_type="div")
+
+    ######################################################################################
+
+    liste_x = []
+    liste_y = []
+    for eintrag in neue_liste:
+        liste_x_add = eintrag[0][1]
+        liste_x.append(liste_x_add)
+        liste_y_add = eintrag[8][1]
+        liste_y.append(liste_y_add)
+
+    df = pd.DataFrame(dict(
+        Datum=[*liste_x],
+        Muskelgewicht=[*liste_y],
+    ))
+    df = df.sort_values(by="Datum")
+    fig = px.line(df, x="Datum", y="Muskelgewicht", title="Muskelgewicht in kg")
+
+    fig.update_layout(autotypenumbers='convert types')
+
+    div7 = plot(fig, output_type="div")
+
+
+
+    auswahl = ["Robin"]
+    auswahl_username = ["grafrob"]
+    ausgewaehlter_username = random.choice(auswahl_username)
+    ausgewaehlter_name = random.choice(auswahl)
+
+
+    return render_template("statistik_jogging.html", barchart_gewicht=div1, barchart_bodyfat=div2, barchart_tbw=div3, barchart_muskeln=div4, barchart_bmi=div5, barchart_fettgewicht=div6, barchart_muskelgewicht=div7, name=ausgewaehlter_name, username=ausgewaehlter_username, seitentitel="Statistik Jogging")
 
 
 
